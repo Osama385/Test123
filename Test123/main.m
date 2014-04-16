2d295912384157ddc25a2a49bda4168462809382
Osama385 <osama.altaf@ebricks-inc.com>
2014-04-16 18:09:50 +0500

    Initial Commit

---
+++ Test123/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Test123
+//
+//  Created by eBricks on 4/16/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
